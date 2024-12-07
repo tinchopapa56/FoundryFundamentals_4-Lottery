2f8f6362030f18f3bf0b6b48978c731850303ba3
Tincho Develops <97058453+tinchopapa56@users.noreply.github.com>
2024-12-07 10:25:04 -0300

    helperConfig for deploy done

--- script/HelperConfig.sol
+++ script/HelperConfig.sol
@@ -2,15 +2,24 @@
 pragma solidity ^0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
-import { VRFCoordinatorV2_5Mock } from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2_5Mock.sol";
+import { VRFCoordinatorV2_5Mock } from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+// import { VRFCoordinatorV2_5Mock } from "@chainlink/"
 //x ahi no funca ese mock
 
 abstract contract CodeConstants {
+    /* VRF MOck*/
+    uint96 public constant MOCK_BASE_FEE = 0.25 ether; 
+    uint96 public constant MOCK_GAS_PRICE_LINK = 1e9; 
+    //LINK / eth price
+    int256 public constant MOCK_WEI_PER_UNIT_LINK = 4e15;
+    /* ENDS VRF MOck*/
+
+
     uint256 public constant ETH_SEPOLIA_CHAIN_ID = 1115511;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
 }
 
-contract HelperConfig is CodeConstants, Script{
+contract HelperConfig is CodeConstants, Script {
 
     error HelperConfig__InvalidChainid();
 
@@ -35,7 +44,7 @@ contract HelperConfig is CodeConstants, Script{
 
         if(hasVrfCoord){
             return networkConfigs[chainId];
-        } else if(isTestingChain){
+        } else if (isTestingChain) {
             return getOrCreateAnvilEthConfig();
         } else {
             revert HelperConfig__InvalidChainid();
@@ -57,5 +66,23 @@ contract HelperConfig is CodeConstants, Script{
         if(localChainAlreadyRunning) {
             return localNetworkConfig;
         }
+
+        vm.startBroadcast();
+        VRFCoordinatorV2_5Mock vrfrMock = new VRFCoordinatorV2_5Mock(
+            MOCK_BASE_FEE,
+            MOCK_GAS_PRICE_LINK, 
+            MOCK_WEI_PER_UNIT_LINK
+        );
+        vm.stopBroadcast();
+
+        localNetworkConfig = NetworkConfig({
+            entranceFee: 0.0001 ether,
+            interval: 30,
+            vrfCoordinator: address(vrfrMock),
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+            subscriptionId: 69563845343696194848808305580506156495437382575977282805424587672873219106065,
+            callbackGasLimit: 500000
+        });
+        return localNetworkConfig;
     }
 }
